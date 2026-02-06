40b70ab54452d125e8d99d4410eef16195484ff0
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-06 15:42:45 -0700

    Add amqp connection in server main func.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatal("Failed to make AMQP connection")
+	}
+	defer conn.Close()
+
+	fmt.Println("AMQP connection successful")
 }
