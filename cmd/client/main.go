5c3e415f69816364d9e11e8b2d44ad3cf4fd0825
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-10 14:32:27 -0700

    Create rabbitMQ queue in client main func.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,5 +23,27 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Println("Successfully connected to RabbitMQ")
+	fmt.Println("Peril game client connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Failed to get username: %s", err)
+	}
+
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.SimpleQueueTransient,
+	)
+	if err != nil {
+		log.Fatalf("Failed to bind to queue: %s", err)
+	}
+	fmt.Printf("Queue %v declared and bound\n", queue.Name)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down Peril.")
 }
