eed4d34d5688ba8017e75ad6562f6a1bda595561
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-10 10:16:50 -0700

    Add rabbitMQ connection to client.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitMQURL = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ")
 }
