7c5bcb6aaf2a70f1e940b0440b01160050d99bea
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-10 14:30:41 -0700

    Create DeclareAndBind func in pubsub package.

---
+++ internal/pubsub/consume.go
@@ -0,0 +1,42 @@
+package pubsub
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	queue, err := channel.QueueDeclare(
+		queueName,
+		queueType == SimpleQueueDurable,
+		queueType == SimpleQueueTransient,
+		queueType == SimpleQueueTransient,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+}
