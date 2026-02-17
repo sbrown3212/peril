bfa33b8643896c67f43e6752db0fe3e5ab983744
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-17 16:28:28 -0700

    Create handlerWar function in cmd/client.

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -46,3 +46,25 @@ func handlerMove(gs *gamelogic.GameState, ch *amqp.Channel) func(gamelogic.ArmyM
 		}
 	}
 }
+
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.Acktype {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.Acktype {
+		defer fmt.Print("> ")
+
+		outcome, _, _ := gs.HandleWar(rw)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
+		default:
+			return pubsub.NackDiscard
+		}
+	}
+}
