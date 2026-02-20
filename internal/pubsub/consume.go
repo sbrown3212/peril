46ba170012a10971745c5518735a82176b43c2f8
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-19 17:09:09 -0700

    Add Qos limit of 10 messages per consumer in subscribe function.

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -75,6 +75,11 @@ func subscribe[T any](
 		return fmt.Errorf("failed to declare and bind queue: %w", err)
 	}
 
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return fmt.Errorf("failed to set Qos: %w", err)
+	}
+
 	msgs, err := ch.Consume(
 		queue.Name, "", false, false, false, false, nil,
 	)
